3048fadeefedb31be75c1d6746eceff79ff79dbb
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2023-11-06 17:00:52 +0900

    updated

--- 231106/Sample01.py
+++ 231106/Sample01.py
@@ -4,13 +4,50 @@ class Student:
     id = None
     sex = None
 
+    def __init__(self, name, age, id, sex) -> None:
+        self.name = name
+        self.age = age
+        self.id = id
+        self.sex = sex
+
+    # getter
     def getName(self): 
         return self.name
+
+    def getAge(self):
+        return self.age
     
-    def setName():
-        print("Do something about it")
+    def getId(self):
+        return self.id
+
+    def getSex(self):
+        return self.sex
+    
+    # setter
+    def setName(self, newName):
+        self.name = newName
+
+    def setAge(self, newAge):
+        self.age = newAge
+
+    def setId(self, id):
+        self.id = id
+
+    def setSex(self, sex):
+        self.sex = sex
+
+    def display(self):
+        print(self.name)
+        print(self.age)
+        print(self.id)
+        print(self.sex)
+
+    
 
 
+s1 = Student("SY", 18, "123456789A", 'F')
+s2 = Student("YG", 20, "123456678B", 'M')
+s1.display()
+s2.display()
 
-sy = Student()
-print(sy.getName())
+
