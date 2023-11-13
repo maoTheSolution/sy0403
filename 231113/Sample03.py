dc3dd7e3da4bf4f65b872f1f81f8b8b95a5c559b
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2023-11-13 17:13:47 +0900

    updated

---
+++ 231113/Sample03.py
@@ -0,0 +1,13 @@
+
+l1 = []
+l2 = list()
+char = 'a'
+for each in range(1, 27):
+    l1.append(each)
+    l2.append(chr(each+96))
+    print(l2)
+
+
+
+def makeDict(list1, list2) -> dict:
+    return dict()
