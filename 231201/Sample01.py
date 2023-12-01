045404d53e5046304cc75c6abcd6c11b827f042b
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2023-12-01 16:51:02 +0900

    updated

--- 231201/Sample01.py
+++ 231201/Sample01.py
@@ -1,7 +1,26 @@
 # pip install xlrd openpyxl pandas
 
 import pandas as pd
+import math
 
 file_name = './10월 월간 메뉴표.xlsx'
-df = pd.read_excel(file_name)
-print(df)
+df = pd.read_excel(file_name, skiprows=[0,1,2,14,15,16])
+
+dropList = []
+week = ['월', '화', '수', '목', '금']
+dish = list()
+for each in df.columns:
+    if each not in week:
+        dropList.append(each)
+
+df = df.drop(columns=dropList)
+
+
+for each in df.index[1::2]:
+    for w in week:
+        if type(df.at[each, w]) == str:
+            dish.append(df.at[each, w])
+
+for each in dish:
+    print(each)
+
