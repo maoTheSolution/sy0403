80755dbf86241b01f7a087b1fcf3132662804439
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2023-12-06 17:02:29 +0900

    updated

--- 231206/Sample02.py
+++ 231206/Sample02.py
@@ -1,5 +1,6 @@
 import pandas as pd
 months = [5, 9, 10, 11]
+dfs = []
 for eachM in months:
     sheets = pd.ExcelFile('./'+str(eachM)+"월 월간 메뉴표.xlsx").sheet_names
     for eachS in sheets:
@@ -7,10 +8,16 @@ for eachM in months:
         # print(df)
         temp = list()
         for eachD in df.columns:
-            if eachD not in  ['월', '화', '수', '목', '금']:
+            if eachD not in ['월', '화', '수', '목', '금']:
                 temp.append(eachD)
         df = df.drop(columns=temp)
-        print(df)
+        dfs.append(df)
+
+for eachDF in dfs:
+    for eachIndex in eachDF.index:
+        for eachD in ['월', '화', '수', '목', '금']:
+            print(eachDF.at[3, eachD])
+        
             
 
 
