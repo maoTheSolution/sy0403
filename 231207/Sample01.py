a51eef39a85771ca861d183ce9da2d45e3e35d89
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2023-12-07 16:54:28 +0900

    updated

---
+++ 231207/Sample01.py
@@ -0,0 +1,17 @@
+import calendar
+
+c = calendar.TextCalendar(calendar.MONDAY)
+year = 2023
+month = 2
+index = 0
+week = ["월", "화", "수", "목", "금"]
+f_week = dict()
+for eachW in week:
+    temp = list()
+    for each in list(c.itermonthdays(year, month))[index::7]:
+        temp.append(each)
+    f_week[eachW] = temp
+    index = index + 1
+
+print(f_week)
+    
