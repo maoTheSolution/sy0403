731dd2264794137acf04b87aa3868cbe60e649cd
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2023-12-15 13:01:33 +0900

    updated

--- Week01/Sample01.py
+++ Week01/Sample01.py
@@ -1,20 +1,65 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import random
+
+class MBTI:
+    mbti = list()
+    result = dict()
+    users = dict()
+
+    def mbtiScore(self):
+        url = 'https://testmoa.com/mbti-compatibility/'
+        page = requests.get(url)
+        soup = bs(page.text, "html.parser")
+        for each in soup.find_all('h3',{'class':'wp-block-heading'}):
+            self.mbti.append(each.text[:4])
+    
+        for each in soup.find_all('figure', {'wp-block-table aligncenter is-style-stripes'}):
+            for matched in [e.text for e in each.find_all('td')[3] if e.text != ', ' and e.text != ',\xa0']:
+                self.result[(each.text[:4], matched.strip())] = 5
+            # 좋은 궁합
+           
+            for matched in [e.text for e in each.find_all('td')[5] if e.text != ', ' and e.text != ',\xa0']:
+                self.result[(each.text[:4], matched.strip())]  = 4
+            # 최악의 궁합
+            for matched in [e.text for e in each.find_all('td')[7] if e.text != ', ' and e.text != ',\xa0']:
+                self.result[(each.text[:4], matched.strip())]  = 1
 
 
-url = 'https://testmoa.com/mbti-compatibility/'
-page = requests.get(url)
-soup = bs(page.text, "html.parser")
+    def makeUsers(self, num):
+        while(len(self.result) < num):
+            tempName = self.makeName()
+            if tempName not in self.result:
+                self.result[tempName] = self.chooseMBTI()
+
+    def makeName(self) -> str:
+        al = [chr(each) for each in range(97, 123)]
+        name = ""
+        for each in range(5):
+            name = name + al[random.randint(0, 25)]
+
+        return name
+    
+
+    def chooseMBTI(self) -> str:
+        temp = random.randint(0, len(self.mbti)-1)
+        return self.mbti[temp]
+    
+    def ready(self):
+        self.mbtiScore()
+        self.makeUsers(10)
+        print(self.result)
+        
+
+    if __name__ == "__main__":
+        ready()
+    
 
 
-# for each in soup.find_all('h3',{'class':'wp-block-heading'}):
-    # print(each.text)
-for each in soup.find_all('figure', {'wp-block-table aligncenter is-style-stripes'}):
-    print(each.text[:4])
-    # 최고의 궁합
-    print("최고의 궁합 : ", [e.text for e in each.find_all('td')[3] if e.text != ', ' and e.text != ',\xa0'])
-    # 좋은 궁합
-    print("좋은 궁합 : ", [e.text for e in each.find_all('td')[5] if e.text != ', ' and e.text != ',\xa0'])
-    # 최악의 궁합
-    print("최악의 궁합 : ", [e.text for e in each.find_all('td')[7] if e.text != ', ' and e.text != ',\xa0'])
-    print("*"*10)
+    
+        
+    
+    
+        
+
+    
