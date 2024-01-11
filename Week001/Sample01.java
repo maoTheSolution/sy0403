abae6d8f9272bd5306bcb46d4b0884c0ad5ede48
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2024-01-11 12:16:16 +0900

    java

---
+++ Week001/Sample01.java
@@ -0,0 +1,9 @@
+public class Sample01{
+    public static void main(String [] args){
+        // System.out.println("Hello");
+        // String temp = "Hello";
+        String temp;
+        temp = 11;
+        System.out.println(temp);
+    }
+}
